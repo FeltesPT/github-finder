6d9c5bc1f27d27fb9e55a513bb7262bac49b18e7
Tiago Dias <tiago.dias@equiis.com>
2019-08-24 09:41:23 +0100

    Now searches for the user

---
+++ src/components/users/Search.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+export class Search extends Component {
+	state = {
+		text: ''
+	};
+
+	onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+	onSubmit = e => {
+		e.preventDefault();
+		this.props.searchUsers(this.state.text);
+		this.setState({ text: '' });
+	};
+
+	render() {
+		return (
+			<div>
+				<form className='form' onSubmit={this.onSubmit}>
+					<input
+						type='text'
+						name='text'
+						placeholder='Search users...'
+						value={this.state.text}
+						onChange={this.onChange}
+					/>
+					<input type='submit' value='Search' className='btn btn-dark btn-block' />
+				</form>
+			</div>
+		);
+	}
+}
+
+export default Search;
