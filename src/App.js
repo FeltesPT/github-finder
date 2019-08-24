24b248cd107e38bf6996afd0f5f87d60ecaac965
Tiago Dias <tiago.dias@equiis.com>
2019-08-24 09:48:42 +0100

    Clear users from state

--- src/App.js
+++ src/App.js
@@ -36,12 +36,19 @@ class App extends React.Component {
 		});
 	};
 
+	// Clear users from state
+	clearUsers = () => this.setState({ users: [], loading: false });
+
 	render() {
 		return (
 			<div className='App'>
 				<Navbar />
 				<div className='container'>
-					<Search searchUsers={this.searchUsers} />
+					<Search
+						searchUsers={this.searchUsers}
+						clearUsers={this.clearUsers}
+						showClear={this.state.users.length > 0}
+					/>
 					<Users loading={this.state.loading} users={this.state.users} />
 				</div>
 			</div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Search extends Component {
 	state = {
 		text: ''
+	};
+
+	static propTypes = {
+		searchUsers: PropTypes.func.isRequired,
+		clearUsers: PropTypes.func.isRequired,
+		showClear: PropTypes.bool.isRequired
 	};
 
 	onChange = e => this.setState({ [e.target.name]: e.target.value });
@@ -26,6 +33,11 @@ export class Search extends Component {
 					/>
 					<input type='submit' value='Search' className='btn btn-dark btn-block' />
 				</form>
+				{this.props.showClear && (
+					<button className='btn btn-light btn-block' onClick={this.props.clearUsers}>
+						Clear
+					</button>
+				)}
 			</div>
 		);
 	}
