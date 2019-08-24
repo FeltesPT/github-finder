b85fbb1582791b6bb6a583d37f500de1ef962566
Tiago Dias <tiago.dias@equiis.com>
2019-08-24 09:21:05 +0100

    Reads the data from github and added spinner

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import Navbar from './components/Layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
+	state = {
+		users: [],
+		loading: false
+	};
+
+	async componentDidMount() {
+		this.setState({ loading: true });
+
+		const res = await axios.get('https://api.github.com/users');
+		this.setState({
+			users: res.data,
+			loading: false
+		});
+	}
+
 	render() {
 		return (
 			<div className='App'>
 				<Navbar />
 				<div className='container'>
-					<Users />
+					<Users loading={this.state.loading} users={this.state.users} />
 				</div>
 			</div>
 		);
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,44 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem';
+import Spinner from '../Layout/Spinner';
+import PropTypes from 'prop-types';
 
-class Users extends Component {
-	state = {
-		users: [
-			{
-				login: 'mojombo',
-				id: 1,
-				avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-				html_url: 'https://github.com/mojombo'
-			},
-			{
-				login: 'defunkt',
-				id: 2,
-				avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-				html_url: 'https://github.com/defunkt'
-			},
-			{
-				login: 'pjhyett',
-				id: 3,
-				avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
-				html_url: 'https://github.com/pjhyett'
-			}
-		]
-	};
-	render() {
-		return (
-			<div style={userStyle}>
-				{this.state.users.map(user => (
-					<UserItem key={user.id} user={user} />
-				))}
-			</div>
-		);
+const Users = ({ users, loading }) => {
+	if (loading) {
+		return <Spinner />;
 	}
-}
+
+	return (
+		<div style={userStyle}>
+			{users.map(user => (
+				<UserItem key={user.id} user={user} />
+			))}
+		</div>
+	);
+};
 
 const userStyle = {
 	display: 'grid',
 	gridTemplateColumns: 'repeat(3, 1fr)',
 	gridGap: '1rem'
+};
+
+Users.propTypes = {
+	user: PropTypes.object.isRequired,
+	loading: PropTypes.bool.isRequired
 };
 
 export default Users;
