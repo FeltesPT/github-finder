cb92edf81cc49b0451140fd48a4fe2403102a5d4
Tiago Dias <tiago.dias@equiis.com>
2019-08-25 15:02:40 +0100

    Cleanup for production

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -5,6 +5,17 @@ import GithubReducer from './githubReducer';
 
 import { SEARCH_USERS, SET_LOADING, CLEAR_USERS, GET_USER, GET_REPOS } from '../types';
 
+let githubClientID;
+let githubClientSecret;
+
+if (process.env.NODE_ENV !== 'production') {
+	githubClientID = process.env.REACT_APP_GITHUB_CLIENT_ID;
+	githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+	githubClientID = process.env.GITHUB_CLIENT_ID;
+	githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
+
 const GithubState = props => {
 	const initialState = {
 		users: [],
@@ -20,7 +31,7 @@ const GithubState = props => {
 		setLoading();
 
 		const res = await axios.get(
-			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+			`https://api.github.com/search/users?q=${text}&client_id=${githubClientID}&client_secret=${githubClientSecret}`
 		);
 
 		dispatch({
@@ -33,7 +44,7 @@ const GithubState = props => {
 		setLoading();
 
 		const res = await axios.get(
-			`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+			`https://api.github.com/users/${username}?client_id=${githubClientID}&client_secret=${githubClientSecret}`
 		);
 
 		dispatch({
@@ -47,7 +58,7 @@ const GithubState = props => {
 		setLoading();
 
 		const res = await axios.get(
-			`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+			`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${githubClientID}&client_secret=${githubClientSecret}`
 		);
 
 		dispatch({
